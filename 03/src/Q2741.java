0229eed9a19ab9ea5c69b8bf172ab4b7e235e419
chappsal <heyhighbyee@gmail.com>
2022-02-17 23:31:29 +0900

    220217

---
+++ 03/src/Q2741.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q2741 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double num = sc.nextDouble();
+		
+		for(int i=1; i<=num; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
